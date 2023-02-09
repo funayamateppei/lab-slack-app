11029f4f270b2f0c2f60c08abe3ac07a7dcc6027
funayamateppei <t.funayama0217@gmail.com>
2023-02-09 20:15:11 +0900

    Breezインストール

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/channels', function (Request $request) {
+    return response()->json([
+        'id' => 1,
+        'uuid' => \Str::uuid(),
+        'name' => 'テストチャンネルの名前',
+        'joined' => true,
+    ]);
+});
+
+Route::get('/me', function () {
+    return response()->json([
+        "id" => 1,
+        "nickname" => "ニックネーム",
+        "email" => "user@example.com",
+        "icon_url" => "http://localhost/users/image/1",
+    ]);
+});
+
+Route::post('/my/icons', function () {
+    return 'http://localhost/users/image/1';
+});
+
+Route::get('/my/channels', function () {
+    return response()->json([
+        [
+            'id' => 1,
+            'uuid' => \Str::uuid(),
+            'name' => 'テストチャンネルの名前1',
+            'joined' => true,
+        ],
+        [
+            'id' => 2,
+            'uuid' => \Str::uuid(),
+            'name' => 'テストチャンネルの名前2',
+            'joined' => false,
+        ],
+    ]);
+});
+
+Route::delete('/channels/{uuid}/messages/{id}', function ($uuid, $id) {
+    return response()->noContent();
 });
